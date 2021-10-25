46e974a29b24541307517f1f8b0ee7e1b05ed99a
Pablo Ogando Ferreira <pabloogando5@gmail.com>
2021-10-25 12:57:39 +0200

    Replacing route component

--- Controller/APIController.php
+++ Controller/APIController.php
@@ -7,12 +7,12 @@ namespace Pumukit\PaellaStatsBundle\Controller;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Pumukit\PaellaStatsBundle\Document\UserAction;
 use Pumukit\PaellaStatsBundle\Services\UserActionService;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/paella")
